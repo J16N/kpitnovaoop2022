860144f1ffd89c7dfb99df958ca59f82af5aa1b3
Tsubasa <j16n@github.com>
2023-02-09 11:50:27 +0530

    Renamed to complex.cpp

---
+++ Assignment-1/complex.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <cmath>
+#include "complex.h"
+
+using namespace std;
+
+Complex::Complex(): real(0), img(0) {}
+	
+Complex::Complex(int r, int i): real(r), img(i) {}
+	
+void Complex::Accept()
+{
+	cout << "Enter real part: ";
+	cin >> real;
+	cout << "Enter imaginary part: ";
+	cin >> img;
+}
+
+void Complex::Display()
+{
+	cout << real << " + " << img << "i" << endl;
+}
+	
+Complex Complex::operator+(Complex &c)
+{
+	return Complex(real + c.real, img + c.img);
+}
+	
+Complex Complex::operator-(Complex &c)
+{
+	return Complex(real - c.real, img - c.img);
+
+}
+Complex Complex::operator++()
+{
+	real = real + 1;
+	img = img + 1;
+	return *this;
+}
+	
+Complex Complex::operator++(int)
+{
+	Complex temp(real, img);
+	real = real + 1;
+	img = img + 1;
+	return temp;
+}
+
+bool Complex::operator==(Complex &c)
+{
+	return ((real == c.real) && (img == c.img));
+}
